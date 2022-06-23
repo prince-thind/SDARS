5be82d8f7df6440e92b0dc5a5ce6d9765b18cce9
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-06-23 13:40:58 +0530

    Update school_sql.py

--- school_sql.py
+++ school_sql.py
@@ -1,6 +1,6 @@
 import mysql.connector as conn
 db=conn.connect(host="127.0.0.1",user="root",password="291005", database="school")
-cursor=db.cursor()
+cursor=db.cursor(buffered=True)
 
 def teachers_log(id,var):
     cursor.execute("select * from faculty where id='{}'".format(id))
@@ -11,6 +11,7 @@ def teachers_log(id,var):
 def students_log(id,var):
     cursor.execute("select * from students where admno='{}'".format(id))
     data=(cursor.fetchone()[-1])
+    print(data)
     if data==var:
         return True
     else: return False
@@ -20,3 +21,19 @@ def parents_log(id,var):
     if data==var:
         return True
     else: return False
+
+
+def get_class(admno:str):
+    global cursor
+    cursor.execute("show tables;")
+    temp=cursor.fetchall()
+    for i in temp:
+        cursor.execute("select * from {} where admno='{}';".format(i[0], admno))
+        data=cursor.fetchall()
+        if len(data)==1:
+            resp= str(i[0])
+            break
+        else:
+            resp= "not in database"
+            continue
+    return resp
