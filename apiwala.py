b81957bf1b2449588897ca6599f6b46de640fc12
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-06-23 13:38:32 +0530

    Update apiwala.py

--- apiwala.py
+++ apiwala.py
@@ -5,11 +5,11 @@ app=FastAPI()
 async def root():
     pass
 
-@app.get("/{personas}/{id}/{variable}")
-async def log(personas:int,id:str, variable:str):
+@app.get('/{personas}/{id}&{variable}/')
+async def root(personas:int,id:str, variable:str):
     if personas==1:
         from school_sql import teachers_log
-        return{'value':teachers_log(id,variable)}
+        return{'value':teachers_log(id, variable)}
     elif personas==2:
         from school_sql import students_log
         return {'value': students_log(id, variable)}
