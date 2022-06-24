0cf4ee695b21f5d5a6befb308dbb4028fd21ce97
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-06-24 13:17:02 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -12,20 +12,20 @@ async def root():
 async def student_assignment(admno:str):
     number=assignments.get_lastassignment_no(get_class(admno))
 
-@app.post("/1/{id}/upload-assignment/{class_name}/")
-async def upload_assignment(id:str,class_name:str,file: bytes = File(default=None)):
-    number=int(assignments.get_lastassignment_no(class_name))
-    path=assignments.make_assignment(class_name,number)["path"]
-    print(path) # idk what is the optput here thats why there is "pass" in conditions
-    if path=="Error":
-        pass
+@app.post("/1/{id}&{var}/upload-assignment/{class_name}/")
+async def upload_assignment(id:str,var:str,class_name:str,file: bytes = File(default=None)):
+    x=login(persona=1,login_data=(tuple(id,var)))["login"]
+    if x==True:
+        number = int(assignments.get_lastassignment_no(self=assignments,class_name=class_name))
+        path = assignments.make_assignment(self=assignments,class_name=class_name, assgno=number)["path"]
+        print(path)  # idk what is the optput here thats why there is "pass" in conditions
+        if path == "Error":
+            pass
+        else:
+            f1 = open(path, "w")
+            file1 = file.decode('UTF-8')
+            f1.write(file1)
+        return {"process":"Completed"}
     else:
-        pass
-        f1 = open(path, "w")
-        file1 = file.decode('UTF-8')
-        f1.write(file1)
-        pass
-
-
-    return {"file_len": len(file)}
+        return {"process":"Failed"}
 
