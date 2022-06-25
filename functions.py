fd3fa508687f5a7a1583ad984bd194dc6a05519e
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-06-25 13:09:04 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,File,UploadFile
+from fastapi.responses import FileResponse
 from database_wala import *
 
 app=FastAPI()
@@ -6,18 +7,36 @@ app=FastAPI()
 async def root():
     pass
 
-#1 -> student, 2 -> student, 3 -> parents
+#1 -> teacher, 2 -> student, 3 -> parents
 
 @app.get("/2/{admno}&{var]/assignments/")
 async def student_assignment(admno:str,var:str):
-    x=login(persona=2,login_data=tuple(admno,var))["login"]
+    x = login(var=var, id=admno, persona=2).data()["login"]
     if x==True:
-        number=assignments.get_lastassignment_no(get_class(admno))
-    else: pass
+        class_name=get_class(admno)
+        number=int(assignments.get_lastassignment_no(class_name))
+        if number<=5:
+            d=get_assignments(0,number,class_name),
+        else:
+            d=get_assignments(number-5,number)
+        return d
+    else:
+        return {"Error":"incorrect id or password"}
+
+@app.post("/2/{admno}&{var}/assignments/{assg_no}/")
+async def open_assignment(admno:str,var:str,assg_no:int):
+    x = login(var=var, id=admno, persona=2).data()["login"]
+    if x==True:
+        path=get_filepath(admno=admno)
+        return {"file":FileResponse(path=path, filename="assignment{}.txt".format(assg_no))}
+    else:
+        return {"Error": "incorrect id or password"}
+
+
 
 @app.post("/1/{id}&{var}/upload-assignment/{class_name}/")
 async def upload_assignment(id:str,var:str,class_name:str,file: bytes = File(default=None)):
-    x=login(persona=1,login_data=(tuple(id,var)))["login"]
+    x = login(var=var, id=id, persona=2).data()["login"]
     if x==True:
         number = int(assignments.get_lastassignment_no(self=assignments,class_name=class_name))
         path = assignments.make_assignment(self=assignments,class_name=class_name, assgno=number)["path"]
@@ -31,4 +50,3 @@ async def upload_assignment(id:str,var:str,class_name:str,file: bytes = File(def
         return {"process":"Completed"}
     else:
         return {"process":"Failed"}
-
