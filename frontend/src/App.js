d3914fb760772b899663f44ba76f6c8ae5b263b4
Prince Thind <princethind799@gmail.com>
2022-08-29 10:26:26 +0530

    implement dummy routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Parents from './pages/parents/Parents';
+import Teachers from './pages/teachers/Teachers';
+import Students from './pages/students/Students';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/parents" element={<Parents />} />
+        <Route path="/teachers" element={<Teachers />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
---
+++ frontend/src/pages/parents/Parents.js
@@ -0,0 +1,3 @@
+export default function Parents() {
+    return <div>parents</div>
+}
