5b1ae1edc507cac37ba174fbbdcd2aa46f0e186e
Prince Thind <princethind799@gmail.com>
2022-11-10 19:51:08 +0530

    implement custom fetch for fake fetch requests

---
+++ frontend/src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    origin: null, //please DO NOT include a traling / in the url, example: http://localhost:8000
+}
+
+export default config;
---
+++ frontend/src/lib/customFetch.js
@@ -0,0 +1,63 @@
+import config from "../config";
+import { fetchFakeAssignments, fetchFakeAttendence, fetchFakeCirculars, fetchFakeFees, fetchFakeLoginResponse, fetchFakeProgress, fetchFakeResults, submitFakeAssignment, submitFakeAttedence, submitFakeProgress, submitFakeResult } from "./fakeAPI";
+
+const origin = config.origin;
+
+export default async function customFetch(route, config) {
+    if (!origin) return await fakeFetch(route, config);
+
+    //todo implement real fetch requests
+}
+
+async function fakeFetch(route, postData) {
+    switch (route) {
+        case '/login/': return fetchFakeLoginResponse({ username: postData.body.username, password: postData.body.password });
+
+        case '/students/circulars/':
+        case '/teachers/circulars/':
+        case '/parents/circulars/': return fetchFakeCirculars();
+
+        case '/teachers/results/': return submitFakeResult({});
+
+        case '/teachers/assignments/': return submitFakeAssignment({});
+
+        case '/teachers/progress/': return submitFakeProgress({})
+
+        case '/teachers/attendence/': return submitFakeAttedence({})
+        case '/parents/attendence/{username}': return null;
+
+        case '/parents/fees/{username}': return null;
+        default: return fetchMethods();
+    }
+
+    function fetchMethods() {
+        if (route.includes('/students/results/')) {
+            return fetchFakeResults()
+        };
+
+        if (route.includes('/students/assignments/')) {
+            return fetchFakeAssignments()
+        };
+
+        if (route.includes('/students/progress/')) {
+            return fetchFakeProgress()
+        };
+
+        if (route.includes('/parents/progress/')) {
+            return fetchFakeProgress()
+        };
+
+        if (route.includes('/students/attendence/')) {
+            return fetchFakeAttendence()
+        };
+
+        if (route.includes('/parents/attendence/')) {
+            return fetchFakeAttendence()
+        };
+
+        if (route.includes('/parents/fees/')) {
+            return fetchFakeFees()
+        };
+
+    }
+}
