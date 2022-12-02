d6ec0bd9ac40ae237519685503e9f102bb6373e9
Sumit Sharma <84061534+Su-nlight@users.noreply.github.com>
2022-12-02 12:24:19 +0530

    Update main_frame.py

--- backend/main_frame.py
+++ backend/main_frame.py
@@ -1,5 +1,8 @@
+import pickle
+
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+import datetime
 
 import database_functions
 import file_functions
@@ -10,7 +13,7 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {"functions status": "login working"}
+    return {"functions status": "working twice"}
 
 
 @app.get("/login/{username}&{password}&{privilege}")
@@ -20,9 +23,8 @@ async def login(username: str, password: str, privilege: str):
     return response
 
 
-# done to the end part just testing left
-
 # -----------------------------------------------------------------------------------------------------------------------
+
 
 @app.get("/students/circulars/{username}")
 async def student_circulars(username: str):
@@ -33,16 +35,13 @@ async def student_circulars(username: str):
         object = file_functions.circulars(student_class=class_n_sec)
         return object.get_data()
 
-
-# done just need to setup circular folder
-
 @app.post("/teachers/circulars/{username}&{s_class}")
 async def teacher_circulars(username: str, s_class: str, file: bytes = File(default=None)):
     try:
         obj = file_functions.circulars(student_class=s_class)
-        path = obj.path+"\\circular_no{}".format(obj.last_circular_no())
-        f = open(path, "w")
-        f.write(file.decode("UTF-8"))
+        path = obj.path+"\\circular_no{}.dat".format(obj.last_circular_no())
+        f = open(path, "wb")
+        pickle.dump(file.decode(utf-8),f)
         status = True
     except:
         status = False
@@ -60,6 +59,7 @@ async def parent_circulars(username: str):
 
 
 # -----------------------------------------------------------------------------------------------------------------------
+
 
 @app.get("/students/assignments/{username}")
 async def student_assignments(username: str):
@@ -84,8 +84,88 @@ async def teacher_assignments(username: str, class_n_sec: str, file: bytes = Fil
     finally:
         return {"status": status}
 
+
 # ======================================================================================================================
 
+
 @app.post("/teachers/progress")
-async def teacher_progress():
-    pass
+async def teacher_progress(username:str,subject_name:str,max_marks:int,marks_obtained:int,description:str):
+    try:
+        f1_obj=open(f"\\students\\{username}.dat",'ab')
+
+        d={"date":(str(datetime.date.today().month),str(datetime.date.today().year)),"subject_name":subject_name,
+           "max_marks":max_marks,"marks_obtained":marks_obtained,"description":description}
+
+        pickle.dump(d.copy(),f1_obj)
+        d.clear()
+        statu=True
+    except:
+        statu=False
+    finally:
+        return {status:statu}
+
+@app.get("/students/progress/{username}")
+async def student_progress(username:str):
+    f1_obj = open(f"\\students\\{username}.dat", 'rb')
+    try:
+        data_multiple=[]
+        while True:
+            data_singular=pickle.load(f1_obj).copy()
+            if data_singular["date"]==(str(datetime.date.today().month),str(datetime.date.today().year)):
+                del data_singular["date"]
+                data_multiple.append(data_singular.copy())
+            else: pass
+
+    except EOFError:
+        reponse={data:data_multiple}
+    finally:
+        return reponse
+
+@app.get("/parents/progress/{username}")
+async def parents_progress(username:str):
+    f1_obj = open(f"\\students\\{username}.dat", 'rb')
+    try:
+        data_multiple = []
+        while True:
+            data_singular = pickle.load(f1_obj).copy()
+            if data_singular["date"] == (str(datetime.date.today().month), str(datetime.date.today().year)):
+                del data_singular["date"]
+                data_multiple.append(data_singular.copy())
+            else:
+                pass
+
+    except EOFError:
+        reponse = {data: data_multiple}
+    finally:
+        return reponse
+
+
+# ======================================================================================================================
+
+
+@app.post("/teachers/attendence")
+async def teachers_attendence(username:str,attendence:int,remarks:str):
+    try:
+        f1_obj = open(f"\\students\\{username}.txt", 'w')
+        f1_obj.write(f"attendence={attendence}\nremarks={remarks}")
+        statu=True
+    except:
+        statu=False
+    finally:
+        return {status:statu}
+
+@app.get("/students/attendence/{username}")
+async def students_attendence(username:str):
+    f1_obj = open(f"\\students\\{username}.txt", 'r')
+    temp=f1_obj.readlines()
+    attendence_d=temp[0].replace("attendence=","")
+    remarks_d=temp[1].replace("remarks=","")
+    return {data:[{name:username,remarks: remarks_d , attendence: int(attendence_d)}]}
+
+@app.get("/parents/attendence/{username}")
+async def parents_assignments(username:str):
+    f1_obj = open(f"\\students\\{username}.txt", 'r')
+    temp = f1_obj.readlines()
+    attendence_d = temp[0].replace("attendence=", "")
+    remarks_d = temp[1].replace("remarks=", "")
+    return {data: [{name: username, remarks: remarks_d, attendence: int(attendence_d)}]}
